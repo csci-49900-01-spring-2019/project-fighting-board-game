d26bccedc1b605425b6d6caf47e2a5003ceead6f
WindCppCoder <35078872+WindCppCoder@users.noreply.github.com>
2019-03-07 14:56:52 -0500

    Add files via upload

---
+++ Weapon List.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponList : MonoBehaviour
+{
+    public List<Weapon> wepList = new List<Weapon>();   //separate weapon tiers by their placement in array, i.e.: tier 1 (0-9) tier 2 (10-19) tier 3 (20-25)
+    public string[] availableNames = new string[3];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        availableNames[0] = "sword";
+        availableNames[1] = "hammer";
+        availableNmaes[2] = "stick";
+        for (int i = 0; i < 25; i++)
+        {
+            int j = Random.Range(0, 2);
+            wepList.Add(new Weapon(availableNames[j]));
+        }
+    }
+
+    public
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public string name;
+    //public string adjective;
+    public int dRangeStart;
+    public int dRangeLimit;
+    public int range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public Weapon(string nam) //can be changed to allow for adjectives after prototyping
+    {
+        name = nam;
+        SetVariance(name);
+    }
+
+    void SetVariance(name)
+    {
+        switch (name)
+        {
+            case "sword":
+                dRangeStart = 10;
+                dRangeLimit = 16;
+                range = 0;
+                break;
+            case "hammer":
+                dRangeStart = 8;
+                dRangeLimit = 18;
+                range = 0;
+                break;
+            case "stick":
+                dRangeStart = 1;
+                dRangeLimit = 6;
+                range = 0;
+                break;
+        } 
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
