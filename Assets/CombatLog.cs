545b50b1b5c994377f97732e68048baf559c599a
Wind CC <wchiachang21@gmail.com>
2019-04-01 13:25:00 -0400

    combatLog made

---
+++ Assets/CombatLog.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombatLog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        combatLog = GameObject.FindObjectOfType<Manager>();
+    }
+    Manager combatLog;
+    // Update is called once per frame
+    void Update()
+    {
+        if (combatLog.combatSystem.called)
+        {
+
+        }
+    }
+}
