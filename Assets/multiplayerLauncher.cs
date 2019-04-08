7309db43f547c06375dbadb2aa8bb66bb23c1af0
Yohji Kusakabe <Ykauji@gmail.com>
2019-04-07 23:02:24 -0400

    lot of networking stuffies
    !!

---
+++ Assets/multiplayerLauncher.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class multiplayerLauncher : Photon.Pun.MonoBehaviourPun
+{
+    public string version = "0.1";
+    
+}
